90ef2d36ec203f2f227714d2f21a54b90c03419c
huawillian <huawillian@gmail.com>
2017-05-11 15:52:41 -0400

    some styling

--- src/client/app/QuestionAdminComponent.jsx
+++ src/client/app/QuestionAdminComponent.jsx
@@ -5,14 +5,15 @@ function QuestionAdminComponent({question, user, handlers}) {
   //var userHasVoted = question.usersVoted.includes(user.username);
   var isAdmin = user.role === 'admin';
   var isAdminKeep = question.keep; // should populate from database
-  var keepColor = isAdminKeep ? 'lightblue' : 'white';
+  var keepColor = isAdminKeep ? '#2b9fda' : 'white';
+  var keepColor2 = isAdminKeep ? 'white' : 'black';
   var keepHandler = isAdminKeep ? handlers.unkeep : handlers.keep; // should get handler from question component
 
   if(isAdmin) {
     return (
       <div className="question-admin-header">
         <span className="question-admin-title"> Townhall #{question.townHall} </span>
-        <button style={{backgroundColor: keepColor}} className="question-admin-button" onClick={() =>keepHandler(question)}> Keep </button>
+        <button style={{backgroundColor: keepColor, color: keepColor2}} className="question-admin-button" onClick={() =>keepHandler(question)}> Keep </button>
       </div>
     )
   } else {
