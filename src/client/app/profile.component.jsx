4a4f1a0471df4f56ed68af214ab033c99ae6eb01
Hack Reactor Student <student@hackreactor.com>
2017-05-12 19:23:51 -0400

    finish user profile tab (no styles)

--- src/client/app/profile.component.jsx
+++ src/client/app/profile.component.jsx
@@ -11,7 +11,9 @@ class ProfileComponent extends React.Component {
 					user: {},
 					open: false,
 				 	updatedUser: {},
-					disableButton: true
+					disableButton: true,
+					questions: [],
+					votes: 0
 				}
 
 			document.cookie.split(';').forEach((str) => {
@@ -23,6 +25,8 @@ class ProfileComponent extends React.Component {
 				}
 			});
 			this.getCurrentUser();
+			this.getTownHall();
+			this.getQuestions();
 
 			this.handleOpen = this.handleOpen.bind(this);
 			this.handleClose = this.handleClose.bind(this);
@@ -65,6 +69,28 @@ class ProfileComponent extends React.Component {
       );
   }
 
+	getQuestions() {
+    fetch('/api/questions', { credentials: 'include' })
+      .then((res) => {
+        if (res.status === 200 || res.status === 304) {
+          return res.json();
+        } else if (res.status === 403) {
+          return null;
+        }
+      })
+      .then(questions => {
+					questions = questions.filter(question => question.cohort === this.state.user.cohort && question.username ===  this.state.user.username);
+					questions = questions.sort((a, b) => a.createdAt > b.createdAt);
+					questions = questions.sort((a, b) => a.townHall < b.townHall);
+					var votes = questions.reduce(function(acc, question) {
+						return acc + question.votes;
+					}, 0);
+					this.setState({questions: questions});
+					this.setState({votes: votes});
+			});
+
+  }
+
 	checkDisabled() {
 		var disabled = true;
 		if(this.state.updatedUser.givenName || this.state.updatedUser.avatarURL) disabled = false;
@@ -102,6 +128,21 @@ class ProfileComponent extends React.Component {
 		})
 	}
 
+	getTownHall() {
+      const props = this.props;
+      fetch('/api/townhall', { credentials: 'include' })
+        .then((res) => {
+          if (res.status === 200 || res.status === 304) {
+            return res.json();
+          } else if (res.status === 403) {
+            return null;
+          }
+        })
+        .then(res => {
+            this.setState({townHall: res.townHall})
+        });
+    }
+
 	render() {
 
 		return (
@@ -109,6 +150,8 @@ class ProfileComponent extends React.Component {
 			<div className="app-body">
 			<div className="profileContent">
 				<img className="profilePic" src={this.state.user.avatarURL}/>
+
+
 
 				<div className="profileHeading">Username</div>
 				<div className="profileText"> {this.state.user.username} </div>
@@ -122,7 +165,52 @@ class ProfileComponent extends React.Component {
 				<div className="profileHeading">Cohort</div>
 				<div className="profileText"> {this.state.user.cohort} </div>
 
-				<button onClick={this.handleOpen}>EDIT PROFILE</button>
+				<div className="profileHeading">TownHall</div>
+				<div className="profileText"> {this.state.townHall} </div>
+
+				<button onClick={this.handleOpen}>EDIT PROFILE FUCKERS!</button>
+
+				<div className="profileHeading">Total Votes</div>
+				<div className="profileText"> {this.state.votes} </div>
+
+				<div className="profileHeading">Questions Asked</div>
+				<div className="profileText"> {this.state.questions.length} </div>
+
+				<div className="profileHeading">Questions</div>
+				{this.state.questions.map((question, idx) => {
+					return (
+						<div key={idx} className="question-wrapper" style={{border: '1px solid black', 'background-color': 'lightblue'}}>
+			        {/* <QuestionAdminComponent question={question} user={user} handlers={handlers}/> */}
+							<p className="question-created">{`TownHall #${question.townHall}`}</p>
+			        <div className="question-header-container">
+			          <div className="question-header">
+			            <div className="question-text">
+			              {question.questionText.split('\n').map(function(line, idx) {
+			                return <p key={idx}>{line}<br/></p>
+			              })}
+			            </div>
+			            <div className="question-info">
+			              {/* <QuestionVoteComponent question={question} user={user} handlers={handlers}/> */}
+										<div className="question-upvote">
+								      <p>{question.votes}</p>
+								      <i className="material-icons">star</i>
+								    </div>
+			            </div>
+			          </div>
+			          <p className="question-created">{moment(question.createdAt).fromNow()}</p>
+			          {question.answered ? <span className="question-badge answered">Answered</span> : null}
+			          <div className="question-tags">
+			            {question.tags.map(function(tag, idx) {
+			              return <span key={idx} className="question-badge">{tag}</span>
+			            })}
+			          </div>
+			        </div>
+			        {/* <QuestionMenuComponent question={question} user={user} handlers={handlers}/> */}
+							<br></br>
+							<br></br>
+			      </div>
+					)
+				})}
 
 				<Dialog
           title="Edit Question"
